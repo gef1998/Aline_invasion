ae6b90f436b91eb5c7d2df36f4d0d9991f1060f7
gef1998 <763377479@qq.com>
2023-11-18 22:39:50 +0800

    1. 解决游戏屏幕长高顺序错误的问题 2. 增加创建一群外星人功能

--- alien.py
+++ alien.py
@@ -19,7 +19,9 @@ class Alien(Sprite):
 
         # 存储外星人的精确水平位置。
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
-    def blitme(self):
-        """在指定位置绘制飞船。"""
+
+    def darw(self):
+        """在指定位置绘制外星人。"""
         self.screen.blit(self.image, self.rect)
