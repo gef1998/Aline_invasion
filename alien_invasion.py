7e64c923b5f8cf553cccd250c9a8fa53916dc299
gef1998 <763377479@qq.com>
2023-11-22 21:58:33 +0800

    mod: 添加主界面按钮和处理逻辑
    - 引入Button类，用于创建按钮对象
    - 添加Play按钮，并处理开始游戏的逻辑
    - 添加Fullscreen按钮，并处理全屏切换的逻辑
    - 添加_check_button方法，处理按钮的点击事件
    
    描述：这次提交在AlienInvasion类中进行了修改，引入了Button类，用于创建按钮对象。添加了处理Play按钮点击事件的方法，以及Fullscreen按钮，并处理了全屏切换的逻辑。这为游戏界面交互提供了新的元素。

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,7 +10,7 @@ from time import sleep
 from settings import Settings
 from ship import Ship
 from game_stats import GameStats
-
+from button import Button
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -20,20 +20,21 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         self.stats = GameStats(self)
-        fullscreen_choice  = input("全屏吗？(yes or no)").lower()
-        assert fullscreen_choice in ['yes', 'no'], "无效的选项，请输入 'yes' 或 'no'"
-        if fullscreen_choice == 'no':
+        if not self.settings.is_fullscreen:
             self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-        if fullscreen_choice == 'yes':
+        else:
             self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-            self.settings.screen_width = self.screen.get_rect().width
-            self.settings.screen_height = self.screen.get_rect().height
+
+        self.screen_rect = self.screen.get_rect()
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
         self._create_fleet()
-
+        self.play_button = Button(self, "Play")
+        self.fullscreen_button = Button(self, "fullscreen")
+        self.fullscreen_button.msg_image_rect.y -= 100
+        self.fullscreen_button.rect.y -= 100
 
     def _check_events(self):
         """响应鼠标和按键事件。"""
@@ -44,6 +45,19 @@ class AlienInvasion:
                 self._check_keyup_events(event)
             elif event.type == pygame.KEYDOWN:         
                 self._check_keydown_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:  # 难道不应该是按下时按钮变形，鼠标松开时判断鼠标是否在坐标内吗？ 还有开始游戏后继续鼠标点击按钮区域还是会进行判断
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_button(mouse_pos)
+
+    def _check_button(self, mouse_pos):
+        """在玩家单击Play按钮时开始新游戏。
+           在玩家单击fullscreen按钮时全屏。
+        """
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
+        if self.fullscreen_button.rect.collidepoint(mouse_pos):
+            Settings.is_fullscreen = True
+            self.__init__()
 
     def _check_keyup_events(self, event): 
         """响应松开。"""
@@ -72,8 +86,8 @@ class AlienInvasion:
     def _create_fleet(self):
         """ 创建外星人群。"""
         alien = Alien(self)
-        available_space_x = self.settings.screen_width - (2 * alien.rect.width)
-        available_space_y = self.settings.screen_height - (3 * alien.rect.height) - self.ship.rect.height
+        available_space_x = self.screen_rect.width - (2 * alien.rect.width)
+        available_space_y = self.screen_rect.height - (3 * alien.rect.height) - self.ship.rect.height
         number_aliens_x = available_space_x // (2 * alien.rect.width)
         number_rows = available_space_y // (2 * alien.rect.height)
         for row_number in range(number_rows):
@@ -143,11 +157,14 @@ class AlienInvasion:
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
-        self.aliens.draw(self.screen)
-
-        for bullet in self.bullets.sprites():
-            bullet.blit_bullet()
+        if not self.stats.game_active:
+            self.play_button.draw_button()
+            self.fullscreen_button.draw_button()
+        else:
+            self.ship.blitme()
+            self.aliens.draw(self.screen)
+            for bullet in self.bullets.sprites():
+                bullet.blit_bullet()
         # 让最近绘制的屏幕可见。
         pygame.display.flip()
 
